1dc5fe6e5843dc904e6977a5d82fa7715a673c6d
Liam Elliott <liamelliott1123@gmail.com>
2018-09-06 14:30:50 -0600

    Remove Initialize Case from Instance Tests

--- spec/visit_spec.rb
+++ spec/visit_spec.rb
@@ -1,10 +1,21 @@
 require "visit"
 
 describe Visit do
-  describe "when object is initialized" do
+
+  describe ".initialize" do
+    it "correctly responds to :new" do
+      expect(described_class).to respond_to(:new).with_keywords(:facility)
+    end
+  end
+
+  context "when object is instance" do
+
+    subject { Visit.new(facility: Visit::Facility.new("CH", "Canmore Clubhouse")) }
+
     it "responds to messages" do
-      expect(subject).to respond_to(:new)
       expect(subject).to respond_to(:facility)
+      expect(subject).to respond_to(:start_date)
+      expect(subject).to respond_to(:number_of_nights)
     end
   end
 end
