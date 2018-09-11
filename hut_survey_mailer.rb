370492135ca5e6a2b0087110bd03ac9b3424f0e6
Liam Elliott <liamelliott1123@gmail.com>
2018-09-11 15:08:23 -0600

    Add Entry Point Script

---
+++ hut_survey_mailer.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby 
+# Hut Survey Mailer
+# 
+# The Alpine Club of Canada is a non-profit organization that promotes
+# mountain culture and involvement in mountain activities. The Club manages
+# and handles bookings for over 30 backcountry facilities.
+#
+# This application emails facility experience surveys to guests at a given
+# interval after their stay has completed.
+#
+# Usage:
+#
+# Run the script from the command line or through a job scheduling service such as cron.
+# All parameters enter through the `config/config.yml` file. If it doesn't exist, copy the 
+# `config/config.defaults.yml` file over and make sure to set your production variables.
+#
