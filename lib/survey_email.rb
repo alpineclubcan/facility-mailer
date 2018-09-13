baec421c210087fbb34facf8d268cefcb1b46f17
Liam Elliott <liamelliott1123@gmail.com>
2018-09-13 11:35:04 -0600

    Add Attributes and Immutability to SurveyEmail

--- lib/survey_email.rb
+++ lib/survey_email.rb
@@ -2,5 +2,13 @@ class SurveyEmail
   attr_reader :recipient, :visit, :template
 
   def initialize(recipient:, visit:, template:)
+    @recipient = recipient
+    @visit = visit
+    @template = template
+    freeze
+  end
+
+  def mail
+
   end
 end
