6f1083187d2b36325ac635258e1a0e4cc0bae5ac
Liam Elliott <liamelliott1123@gmail.com>
2018-09-19 14:42:18 -0600

    Add Duck Types for Different Names Including Empty

--- lib/guest.rb
+++ lib/guest.rb
@@ -1,8 +1,56 @@
 class Guest
 
-  Name = Struct.new(:first, :last) do
+  EmptyName = Struct.new(:name) do
+    def initialize
+      super('ACC Facility User')
+    end
+
+    def formal
+      name
+    end
+
+    def informal
+      name
+    end
+
+    def to_s
+      name
+    end
+
+    def to_name
+      self
+    end
+  end
+
+  OrganizationName = Struct.new(:name) do
+    def formal
+      name
+    end
+
+    def informal
+      name
+    end
+
+    def to_s
+      name
+    end
+
+    def to_name
+      self
+    end
+  end
+
+  IndividualName = Struct.new(:first, :last) do
     def full
       "#{first} #{last}"
+    end
+
+    def formal
+      full
+    end
+
+    def informal
+      first
     end
 
     def to_s
@@ -31,9 +79,13 @@ class Guest
 
   attr_reader :name, :email
 
-  def initialize(name:, email:)
+  def initialize(name: Guest::EmptyName.new, email:)
     @name = name.to_name
     @email = email.to_email
     freeze
+  end
+
+  def to_guest
+    self
   end
 end
