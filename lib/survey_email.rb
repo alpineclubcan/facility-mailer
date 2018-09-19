01195a316c3ddecb0610a1a91da1fcfca70975a0
Liam Elliott <liamelliott1123@gmail.com>
2018-09-19 17:16:41 -0600

    Add Rendering to Survey Email

--- lib/survey_email.rb
+++ lib/survey_email.rb
@@ -1,15 +1,34 @@
 require 'erb'
+require 'mail'
 
 class SurveyEmail
-  Template = Struct.new(:html, :text)
+  FROM = 'confirmations@alpineclubofcanada.ca'.freeze
 
-  attr_reader :visit
+  Template = Struct.new(:html, :text) do
+    def to_template
+      self
+    end
+  end
 
-  def initialize(visit:)
-    @visit = visit
+  def initialize(visit:, template:)
+    @visit = visit.to_visit
+    @template = template.to_template
     freeze
   end
 
-  def render(template:)
+  def render
+    Mail.new do |m|
+      m.from FROM
+      m.to visit.guest.email.to_s
+      m.subject 'Thank you for staying with us!'
+
+      m.text_part { |msg| msg.body ERB.new(template.text).result(binding) }
+      m.html_part { |msg| msg.body ERB.new(template.html).result(binding) }
+    end
   end
+
+  private
+
+  attr_reader :visit, :template
+
 end
