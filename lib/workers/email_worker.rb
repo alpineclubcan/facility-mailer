f6ae6a6e6b44049c20bc1b808bc730dae1914c66
Liam Elliott <liamelliott1123@gmail.com>
2018-09-19 17:18:34 -0600

    Change Worker to Accept Mail::Message

--- lib/workers/email_worker.rb
+++ lib/workers/email_worker.rb
@@ -5,16 +5,9 @@ class Emailer
   include Sneakers::Worker
   from_queue 'emails'
 
-  def work(msg)
-
-    Mail.deliver do
-      to msg.recipient
-      from 'confirmations@alpineclubofcanada.ca'
-      subject "Tell us about your stay!"
-
-      text_part { body 'One line of text content' }
-      html_part { body '<h1>Html email content!</h1>' }
-    end
+  def work(email)
+    message = email.render
+    message.deliver!
 
     ack!
   end
