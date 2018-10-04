5e9ab293c0d192d87b22acb0be0bc824022f8fda
Liam Elliott <liamelliott1123@gmail.com>
2018-10-04 09:36:50 -0600

    Change Logging Side Effect for Email Delivery

--- merlin.rb
+++ merlin.rb
@@ -1,7 +1,5 @@
 # Hide fugly procedural database access code in here!
 
-require 'ostruct'
-
 require Pathname(__dir__).join('lib', 'guest.rb')
 require Pathname(__dir__).join('lib', 'visit.rb')
 
@@ -37,25 +35,24 @@ module Merlin
     visits
   end
 
-  def self.log_email_for_visit(db:, email:)
+
+  def self.deliver_survey_email(db:, email:)
+    message = email.render
+
     begin
-      conn = PG::connect(db)
+      message.deliver!
 
-      visit = email.visit
-      
-      res = conn.exec_params(
-        'INSERT INTO public.hut_survey(reservation_id, email, end_date, facility_code, date_sent) VALUES($1::integer, $2::varchar, $3::date, $4::varchar, $5::date)', 
-        [visit.reservation_id, visit.guest.email, visit.end_date, visit.facility.code, Date::today]
-      )
+      if message.bounced?
+        puts "Email to #{email.visit.guest.email} bounced at #{fnow} and may not be delivered."
+      else
+        puts "Email successfully delivered to #{email.visit.guest.email} at #{fnow}."
+      end
 
-    rescue PG::Error => e  
-      puts "An error of type #{e.class} occurred at #{fnow} while logging survey to the database."
-      
-    ensure
-      conn&.close
+      log_email_for_visit(db: db, email: email)
+
+    rescue => e
+      puts "An error of type #{e.class} occurred at #{fnow} while attempting to deliver the survey email."
     end
-
-    "Survey email successfully logged for #{visit.guest.email} at #{fnow}"
   end
 
   def self.hut_survey_for_visit(db:, email:)
@@ -71,7 +68,6 @@ module Merlin
       hut_survey = res.first
 
     rescue PG::Error => e
-      puts e.message
       puts "An error of type #{e.class} occurred at #{fnow} while getting hut survey from database."
 
     ensure
@@ -84,6 +80,27 @@ module Merlin
   private
 
   DATE_FORMAT = '%H:%M:%S %Y-%m-%d'.freeze
+
+  def self.log_email_for_visit(db:, email:)
+    begin
+      conn = PG::connect(db)
+
+      visit = email.visit
+      
+      res = conn.exec_params(
+        'INSERT INTO public.hut_survey(reservation_id, email, end_date, facility_code, date_sent) VALUES($1::integer, $2::varchar, $3::date, $4::varchar, $5::date)', 
+        [visit.reservation_id, visit.guest.email, visit.end_date, visit.facility.code, Date::today]
+      )
+
+      puts "Email logged for #{email.visit.guest.email} at #{fnow}."
+
+    rescue PG::Error => e  
+      puts "An error of type #{e.class} occurred at #{fnow} while logging survey to the database."
+      
+    ensure
+      conn&.close
+    end
+  end
 
   def self.fnow
     now.strftime(DATE_FORMAT)
