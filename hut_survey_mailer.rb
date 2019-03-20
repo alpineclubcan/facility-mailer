15783e8c44e882da81b520375121e9765c31789a
Liam Elliott <liamelliott1123@gmail.com>
2019-03-20 09:41:27 -0600

    Add Skip Functionality for Emails

--- hut_survey_mailer.rb
+++ hut_survey_mailer.rb
@@ -45,6 +45,8 @@ Mail.defaults do
 end
 
 CONFIG.sending_options.each do |option|
+  next if option.skip
+
   email_template = Email::Template.new(template(name: option.template, format: :html), template(name: option.template, format: :txt))
   visits = Merlin::visits_from_days(db: CONFIG.db, delay: option.delay)
   emails = visits.map { |visit| Email.new(visit: visit, subject: option.subject, template: email_template) }
