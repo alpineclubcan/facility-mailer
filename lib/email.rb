d6e0d63830c5cc016c2ff4bb57146886b10c0563
Liam Elliott <liamelliott1123@gmail.com>
2019-03-21 16:05:29 -0600

    Change Email Params to Hash

--- lib/email.rb
+++ lib/email.rb
@@ -1,5 +1,6 @@
 require 'erb'
 require 'mail'
+require 'hash_dot'
 
 class Email
   FROM = 'confirmations@alpineclubofcanada.ca'.freeze
@@ -10,29 +11,28 @@ class Email
     end
   end
 
-  attr_reader :visit, :template
+  attr_reader :options, :data
 
-  def initialize(visit:, subject:, template:)
-    @visit = visit.to_visit
-    @subject = subject.to_s
-    @template = template.to_template
+  def initialize(options: {}.to_dot, data: {}.to_dot)
+    @options = options
+    @data = data
     freeze
   end
 
   def render
     Mail.new do |m|
       m.from FROM
-      m.to visit.guest.email.to_s
-      m.subject subject
+      m.to options.to
+      m.subject options.subject
 
       m.text_part do |msg| 
         msg.content_type CONTENT_TYPE_TEXT
-        msg.body ERB.new(template.text).result(binding) 
+        msg.body ERB.new(options.template.text).result_with_hash(data) 
       end
 
       m.html_part do |msg| 
         msg.content_type = CONTENT_TYPE_HTML
-        msg.body ERB.new(template.html).result(binding)
+        msg.body ERB.new(options.template.html).result_with_hash(data)
       end 
     end
   end
@@ -41,7 +41,5 @@ class Email
 
   CONTENT_TYPE_HTML = 'text/html; charset=utf-8'
   CONTENT_TYPE_TEXT = 'text/plain; charset=utf-8'
-
-  attr_reader :subject
 
 end
