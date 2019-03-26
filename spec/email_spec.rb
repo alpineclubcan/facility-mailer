756f56e5a40ea83f9de259261fd097269b8b5d96
Liam Elliott <liamelliott1123@gmail.com>
2019-03-26 10:15:41 -0600

    Fix Email Keyword Test

--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -7,7 +7,7 @@ describe Email do
   VISIT = Visit.new(reservation_id: 0, facility: FACILITY, guest: Guest.new(email: Guest::EmailAddress.new('mountain@acc.net'), name: Guest::EmptyName.new), number_of_nights: 5)
   TEMPLATE = Email::Template.new('<h1>HTML version</h1>', 'This is text version of email.')
 
-  subject { Email.new(visit: VISIT, subject: 'Really sketchy subject line', template: TEMPLATE) }
+  subject { Email.new(options: { subject: 'Really sketchy subject line', template: TEMPLATE }, data: { visit: VISIT }) }
 
   it "responds to messages" do
     expect(subject).to respond_to(:render)
@@ -15,7 +15,7 @@ describe Email do
 
   describe "#initialize" do
     it "accepts correct keywords" do
-      expect(subject.method(:initialize).parameters).to eq([[:keyreq, :visit], [:keyreq, :subject], [:keyreq, :template]])
+      expect(subject.method(:initialize).parameters).to eq([[:key, :options], [:key, :data]])
     end
 
     it "freezes the object" do
