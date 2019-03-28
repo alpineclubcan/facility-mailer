ae225f97193678f9b8e1ceb971548ab7f84c9545
Liam Elliott <liamelliott1123@gmail.com>
2019-03-28 17:04:44 -0600

    Replace Visit With Itinerary

---
+++ lib/itinerary.rb
@@ -0,0 +1,37 @@
+class Itinerary
+
+  Facility = Struct.new(:code, :name) do
+    def to_facility
+      self
+    end
+  end
+
+  Reservation = Struct.new(:facility, :bookings) do
+    def continuous?
+      # reservation has no gaps and number of users does not change
+    end
+
+    def to_reservation
+      self
+    end
+  end
+
+  Booking = Struct.new(:date, :number_of_users) do
+    def to_booking
+      self
+    end
+  end
+
+  attr_reader :guest, :reservations
+
+  def initialize(:guest, :reservations)
+    @guest = guest.to_guest
+    @reservations = reservations.to_a
+    freeze
+  end
+
+  def to_itinerary
+    self
+  end
+
+end
